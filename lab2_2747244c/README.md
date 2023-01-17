86fb799b399dcddec74b2f82d7cec09e9be90f19
2747244C <122897169+2747244C@users.noreply.github.com>
2023-01-17 15:44:50 +0000

    Upload lab2 files

---
+++ lab2_2747244c/README.md
@@ -0,0 +1,4 @@
+# Lab2_2747244C
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/2747244C/pen/PoBJmdE](https://codepen.io/2747244C/pen/PoBJmdE).
+
---
+++ lab2_2747244c/dist/script.js
@@ -0,0 +1,121 @@
+// The value for 'accessToken' begins with 'pk...'
+mapboxgl.accessToken =
+  "pk.eyJ1IjoiMjc0NzI0NGMiLCJhIjoiY2xjcDhpdjFsMDJmbTN2bzRmeHRsdWg2eSJ9.TBzsgc_mF3JitNmp1x1s8Q";
+
+// Define a map object by initialising a Map from Mapbox
+const map = new mapboxgl.Map({
+  container: "map",
+  // Replace YOUR_STYLE_URL with your style URL.
+  style: "mapbox://styles/2747244c/cld08usmo004g14p2q54w51rg"
+});
+
+
+map.on("mousemove", (event) => {
+  const dzone = map.queryRenderedFeatures(event.point, {
+    layers: ["glasgow-simd"]
+  });
+  document.getElementById("pd").innerHTML = dzone.length
+    ? `<h3>${dzone[0].properties.DZName}</h3><p>Rank: <strong>${dzone[0].properties.Percentv2}</strong> %</p>`
+    : `<p>Hover over a data zone!</p>`;
+  
+      map.getSource("hover").setData({
+    type: "FeatureCollection",
+    features: dzone.map(function (f) {
+      return { type: "Feature", geometry: f.geometry };
+    })
+  });
+});
+  
+
+
+
+map.on("load", () => {
+  //the rest of the code will go in here
+  const layers = [
+    "<10",
+    "20 ",
+    "30 ",
+    "40 ",
+    "50 ",
+    "60 ",
+    "70 ",
+    "80 ",
+    "90 ",
+    "100"
+  ];
+  const colors = [
+    "#67001f",
+    "#ad1f2f",
+    "#d55b48",
+    "#f4a582",
+    "#fddbc7",
+    "#d5e7f1",
+    "#92c5de",
+    "#4393c3",
+    "#2e6a8e",
+    "#2d3e49"
+  ];
+
+  // create legend
+  const legend = document.getElementById("legend");
+
+  layers.forEach((layer, i) => {
+    const color = colors[i];
+    const key = document.createElement("div");
+    //place holder
+    key.className = "legend-key";
+    key.style.backgroundColor = color;
+    key.innerHTML = `${layer}`;
+
+    legend.appendChild(key);
+
+    if (i <= 1 || i >= 8) {
+      key.style.color = "white";
+    }
+  });
+  
+  map.addSource("hover", {
+  type: "geojson",
+  data: { type: "FeatureCollection", features: [] }
+});
+
+map.addLayer({
+  id: "dz-hover",
+  type: "line",
+  source: "hover",
+  layout: {},
+  paint: {
+    "line-color": "black",
+    "line-width": 4
+  }
+});
+
+  
+  
+});
+
+
+map.addControl(new mapboxgl.NavigationControl(), "top-left");
+map.addControl(
+  new mapboxgl.GeolocateControl({
+    positionOptions: {
+      enableHighAccuracy: true
+    },
+    trackUserLocation: true,
+    showUserHeading: true
+  }),
+  "top-left"
+);
+const geocoder = new MapboxGeocoder({
+  // Initialize the geocoder
+  accessToken: mapboxgl.accessToken, // Set the access token
+  mapboxgl: mapboxgl, // Set the mapbox-gl instance
+  marker: false, // Do not use the default marker style
+  placeholder: "Search for places in Glasgow", // Placeholder text for the search bar
+  proximity: {
+    longitude: 55.8642,
+    latitude: 4.2518
+  } // Coordinates of Glasgow center
+});
+
+map.addControl(geocoder, "top-left");
